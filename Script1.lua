5b8d44ac249541c0bb692f72f578492ccce2f3c1
Meline <79465817+Meline900@users.noreply.github.com>
2024-12-28 23:58:52 +0700

    Update Script1.lua

--- Script1.lua
+++ Script1.lua
@@ -145,6 +145,13 @@ local unc = Tab:CreateButton({
    end,
 })
 
+local sunc = Tab:CreateButton({
+   Name = "SUNC test",
+   Callback = function()
+      loadstring(game:HttpGet("https://gitlab.com/sens3/nebunu/-/raw/main/HummingBird8's_sUNC_yes_i_moved_to_gitlab_because_my_github_acc_got_brickedd/sUNCm0m3n7.lua"))()
+   end,
+})
+
 local Tab = Window:CreateTab("Games sripts", "gamepad-2")
 
 
